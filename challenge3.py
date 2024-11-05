2e356b03b8dccd37d5b71086e0d1652da98d7bfe
Courtney344 <courtney.taylor@student.oneidaschools.org>
2024-11-05 18:05:20 +0000

    challenge3.py

---
+++ challenge3.py
@@ -0,0 +1,14 @@
+list = ["r", "r", "g", "b", "g", "g", "b", "b", "g", "r"]
+#red-5  blue-20  green- 10
+
+sum = 0
+
+for x in list:
+    if x == "r":
+        sum = sum + 5
+    elif x == "b":
+        sum = sum + 20
+    elif x == "g":
+        sum = sum + 10
+
+print(sum)
